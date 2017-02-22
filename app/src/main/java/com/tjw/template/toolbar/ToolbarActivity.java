8bc90719554fbc8dc0999f0659041564ab90e46c
tangjw <tjwabc@gmail.com>
2017-02-22 23:55:20 +0800

    用 Toolbar setSupportActionbar() 后一些api 失去效果

--- app/src/main/java/com/tjw/template/toolbar/ToolbarActivity.java
+++ app/src/main/java/com/tjw/template/toolbar/ToolbarActivity.java
@@ -4,6 +4,8 @@ import android.os.Bundle;
 import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
 
 import com.tjw.template.R;
 
@@ -21,27 +23,61 @@ public class ToolbarActivity extends AppCompatActivity {
         
         mToolbar = (Toolbar) findViewById(R.id.toolbar);
         
-        setSupportActionBar(mToolbar);
+        //若完全使用 toolbar menu api, 注释掉即可 
+//        setSupportActionBar(mToolbar);
         
         setToolbar();
     }
+    
+    /*@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // TODO Auto-generated method stub
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_toolbar, menu);
+        return super.onCreateOptionsMenu(menu);
+    }*/
+    
     
     /**
      * 设置Toolbar的 相关api
      */
     private void setToolbar() {
         ActionBar actionBar = getSupportActionBar();
-        
-        if (actionBar == null) {
-            return;
+    
+        System.out.println("actionbar => "+actionBar);
+        if (actionBar != null) {
+            // 显示 title 默认为 true ,设为 false Toolbar上的 title才会显示
+            actionBar.setDisplayShowTitleEnabled(false);
+            // 显示返回箭头 ← 默认不显示
+            actionBar.setDisplayHomeAsUpEnabled(true);
         }
+        mToolbar.setLogo(R.mipmap.ic_launcher);
         
-        actionBar.setDisplayShowTitleEnabled(false);
-        actionBar.setDisplayHomeAsUpEnabled(true);
+        mToolbar.setTitle("我是Toolbar");
         
-//        toolbar.setn
-//        final Drawable upArrow = getResources().getDrawable(android.R.drawable.abc_ic_ab_back_material);
-//        upArrow.setColorFilter(getResources().getColor(R.color.grey), PorterDuff.Mode.SRC_ATOP);
-//        getSupportActionBar().setHomeAsUpIndicator(upArrow);
+        mToolbar.setSubtitle("我是小标题");
+        
+        mToolbar.setNavigationContentDescription("这是什么?");
+        
+        // 设置返回键图标
+        mToolbar.setNavigationIcon(R.drawable.ic_back);
+        
+        mToolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                finish();
+            }
+        });
+
+        mToolbar.inflateMenu(R.menu.menu_toolbar);
+
+        mToolbar.setOverflowIcon(getResources().getDrawable(R.drawable.ic_add));
+        
+        mToolbar.setOnMenuItemClickListener(new Toolbar.OnMenuItemClickListener() {
+            @Override
+            public boolean onMenuItemClick(MenuItem item) {
+                return true;
+            }
+        });
     }
 }
