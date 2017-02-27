6f5217244f0e7eb18c678a3116bc036c7b5ebbb2
tjwabc@gmail.com <tjwdegit0306>
2017-02-27 18:24:58 +0800

    RxJava2从头学起

---
+++ app/src/main/java/com/tjw/template/rxbus/BaseActivity.java
@@ -0,0 +1,21 @@
+package com.tjw.template.rxbus;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+
+/**
+ * Created by Android on 2017/2/27.
+ */
+
+public class BaseActivity extends AppCompatActivity {
+    
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+//        initRxBus();
+    }
+    
+    
+}
