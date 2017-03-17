23716832cd58829307a800d368749b1b599a82b4
tangjw <tjwabc@gmail.com>
2017-03-17 18:10:03 +0800

    调相册

--- app/src/main/java/com/tjw/template/camera/CameraActivity.java
+++ app/src/main/java/com/tjw/template/camera/CameraActivity.java
@@ -2,70 +2,134 @@ package com.tjw.template.camera;
 
 import android.content.Intent;
 import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.net.Uri;
 import android.os.Bundle;
-import android.provider.MediaStore;
+import android.os.Environment;
+import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.view.View;
 import android.widget.ImageView;
 
+import com.jph.takephoto.app.TakePhoto;
+import com.jph.takephoto.app.TakePhotoImpl;
+import com.jph.takephoto.model.CropOptions;
+import com.jph.takephoto.model.InvokeParam;
+import com.jph.takephoto.model.TContextWrap;
+import com.jph.takephoto.model.TResult;
+import com.jph.takephoto.permission.InvokeListener;
+import com.jph.takephoto.permission.PermissionManager;
+import com.jph.takephoto.permission.TakePhotoInvocationHandler;
 import com.tjw.template.R;
 import com.tjw.template.swipeback.BaseActivity;
+
+import java.io.File;
 
 /**
  * ^-^
  * Created by tang-jw on 2017/3/16.
  */
 
-public class CameraActivity extends BaseActivity {
+public class CameraActivity extends BaseActivity implements TakePhoto.TakeResultListener, InvokeListener {
     
     private static final int TAKE_PHOTO = 201;
     private static final int TAKE_ALBUM = 202;
     private ImageView mImageView;
     
+    private InvokeParam mInvokeParam;
+    
+    private TakePhoto mTakePhoto;
+    
+    @Override
+    protected void beforeSuperCreate(@Nullable Bundle savedInstanceState) {
+        super.beforeSuperCreate(savedInstanceState);
+        getTakePhoto().onCreate(savedInstanceState);
+    }
+    
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        getTakePhoto().onSaveInstanceState(outState);
+        super.onSaveInstanceState(outState);
+    }
+    
     @Override
     protected void initView(Bundle savedInstanceState) {
         setContentView(R.layout.activity_camera);
-        
         mImageView = (ImageView) findViewById(R.id.imageView);
+        
     }
     
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
-        super.onActivityResult(requestCode, resultCode, data);
-        if (resultCode != RESULT_OK) return;
-        
-        switch (requestCode) {
-            case TAKE_PHOTO:
-                Bundle extras = data.getExtras();
-                Bitmap bitmap = (Bitmap) extras.get("data");
-                mImageView.setImageBitmap(bitmap);
-                
-                break;
-            
-            case TAKE_ALBUM:
-//                Bundle extras1 = data.getExtras();
-//                Bitmap bitmap1 = (Bitmap) extras1.get("data");
-//                mImageView.setImageBitmap(bitmap1);
-                Uri data1 = data.getData();
+        getTakePhoto().onActivityResult(requestCode, resultCode, data);
+    }
     
-                System.out.println(data1);
-    
-                break;
-        }
-        
-        
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionManager.TPermissionType type = PermissionManager.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        PermissionManager.handlePermissionsResult(this, type, mInvokeParam, this);
     }
     
     public void openCamera(View view) {
-        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
-        startActivityForResult(intent, TAKE_PHOTO);
+        File file = new File(Environment.getExternalStorageDirectory(), "/temp/" + System.currentTimeMillis() + ".jpg");
+        Uri imageUri = Uri.fromFile(file);
+        mTakePhoto.onPickFromCaptureWithCrop(imageUri,getCropOptions());
+       
+    }
+    
+    public void openAlbum(View view) {
+        mTakePhoto.onPickMultipleWithCrop(1,getCropOptions());
+    }
+    
+    @Override
+    public void takeSuccess(TResult result) {
+        
+        System.out.println("takeSuccess: " + result.getImages().get(0).getOriginalPath());
+        
+        Bitmap bitmap = BitmapFactory.decodeFile(result.getImages().get(0).getOriginalPath());
+        mImageView.setImageBitmap(bitmap);
         
         
     }
     
-    public void openAlbum(View view) {
-        Intent intent = new Intent(Intent.ACTION_PICK,null);
-        intent.setDataAndType(MediaStore.Images.Media.EXTERNAL_CONTENT_URI, "image/*");
-        startActivityForResult(intent, TAKE_ALBUM);
+    @Override
+    public void takeFail(TResult result, String msg) {
+        System.out.println("takeFail");
     }
+    
+    @Override
+    public void takeCancel() {
+        System.out.println("takeCancel");
+    }
+    
+    @Override
+    public PermissionManager.TPermissionType invoke(InvokeParam invokeParam) {
+        PermissionManager.TPermissionType type = PermissionManager.checkPermission(TContextWrap.of(this), invokeParam.getMethod());
+        if (PermissionManager.TPermissionType.WAIT.equals(type)) {
+            this.mInvokeParam = invokeParam;
+        }
+        return type;
+    }
+    
+    /**
+     * 获取TakePhoto实例
+     *
+     * @return
+     */
+    public TakePhoto getTakePhoto() {
+        if (mTakePhoto == null) {
+            mTakePhoto = (TakePhoto) TakePhotoInvocationHandler.of(this).bind(new TakePhotoImpl(this, this));
+        }
+        return mTakePhoto;
+    }
+    
+    private CropOptions getCropOptions(){
+        CropOptions.Builder builder=new CropOptions.Builder();
+        builder.setOutputX(600).setOutputY(600);
+        builder.setAspectX(1).setAspectY(1);
+        builder.setWithOwnCrop(false);
+        return builder.create();
+    }
+    
 }
--- app/src/main/java/com/tjw/template/swipeback/BaseActivity.java
+++ app/src/main/java/com/tjw/template/swipeback/BaseActivity.java
@@ -32,24 +32,25 @@ public abstract class BaseActivity extends AppCompatActivity
     protected void onCreate(@Nullable Bundle savedInstanceState) {
         // 在 super.onCreate(savedInstanceState) 之前调用该方法
         initSwipeBackFinish();
+        beforeSuperCreate(savedInstanceState);
         super.onCreate(savedInstanceState);
         
         // 将BaseActivity 添加到栈管理
         ActivityStackUtils.getInstance().addActivity(this);
         
-        beforeSetLayout();
         
         initView(savedInstanceState);
         
         setListener();
-    
+        
         loadData();
         
     }
     
-    protected void loadData(){}
+    protected void loadData() {
+    }
     
-    protected void beforeSetLayout() {
+    protected void beforeSuperCreate(@Nullable Bundle savedInstanceState) {
         if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.LOLLIPOP) {
             getWindow().getDecorView().setSystemUiVisibility(View.SYSTEM_UI_FLAG_LAYOUT_FULLSCREEN);
         } else if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
