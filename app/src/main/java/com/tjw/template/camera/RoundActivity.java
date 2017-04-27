5f673f565835692f203d8b1c785f9e04e520e341
tangjw <tjwabc@gmail.com>
2017-04-27 18:09:28 +0800

    mvp

---
+++ app/src/main/java/com/tjw/template/camera/RoundActivity.java
@@ -0,0 +1,20 @@
+package com.tjw.template.camera;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+import com.tjw.template.R;
+
+/**
+ * ^-^
+ * Created by tang-jw on 2017/4/27.
+ */
+
+public class RoundActivity extends AppCompatActivity {
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_round);
+    }
+}
